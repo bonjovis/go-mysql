9a91af84e174b6b09c9321346a68f57f32976752
bonjovis <bonjovis1985@gmail.com>
2017-03-17 17:27:00 +0800

    Update escape.go

--- escape.go
+++ escape.go
@@ -1,3 +1,15 @@
+/*
+*
+* Author: Hui Ye - <bonjovis@163.com>
+*
+* Last modified: 2017-03-17 09:21
+*
+* Filename: escape.go
+*
+* Copyright (c) 2016 JOVI
+*
+ */
+
 package mysql
 
 func EscapeString(sql string) string {
